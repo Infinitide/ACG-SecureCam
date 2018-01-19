f5b420c8b5b934b330355e35d09b56290634df07
Infinitide <yongze.dism@gmail.com>
2018-01-20 00:11:03 +0800

    v2
    Added Asymmetric Authentication
    Added KeyGen

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 [![Build Status](https://travis-ci.com/Infinitide/ACG-SecureCam.svg?token=VjEYc68MUWgPSpWqgDNV&branch=dev)](https://travis-ci.com/Infinitide/ACG-SecureCam)
 
 ACG CA2 Repository
+
+## Setup
+1. Compile and run file [bin/keys/initKeyPair](bin/keys/initKeyPair)
+2. Copy (private key)[bin/keys/KeyPair/private.key] to [bin/Server](bin/Server)
+3. Copy (public key)[bin/keys/KeyPair/public.key] to [bin/Client](bin/Client)
+4. Compile and run [Server](bin/Server/Server.java)
+5. Compile and run [Client](bin/Client/Client.java)
--- bin/Client/Client.java
+++ bin/Client/Client.java
@@ -16,56 +16,49 @@ import java.nio.file.*;
 import javax.crypto.*;
 import javax.crypto.spec.*;
 import java.util.*;
+import java.security.spec.*;
 
 public class Client { 
 
 	private static ByteArrayOutputStream CACHE;
+	private static final String PUBLICKEY = "public.key";
 	
 	private static void cache(byte[] bytes){
 		try {
 			ByteArrayOutputStream baos = new ByteArrayOutputStream(bytes.length);
-				baos.write(bytes, 0, bytes.length);
-				CACHE = baos;
-
+			baos.write(bytes, 0, bytes.length);
+			CACHE = baos;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	private static boolean verify(byte[] ehmac){
-		DataInputStream in = new DataInputStream(new ByteArrayInputStream(CACHE.toByteArray()));;
-		String keyFile = "key.txt";
-		String encodedKey = null;
+	private static boolean verifySignature(byte[] signature) throws Exception {
+		Signature sig = Signature.getInstance("SHA1withRSA");
+		byte[] keyBytes = Files.readAllBytes(new File(PUBLICKEY).toPath());
+		X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
+		KeyFactory kf = KeyFactory.getInstance("RSA");
+		sig.initVerify(kf.generatePublic(spec));
+		sig.update(CACHE.toByteArray());
+		return sig.verify(signature);
+	}
 
-		byte[] result = null;
-		try {
-			FileReader fread = new FileReader(keyFile);
-			BufferedReader bread = new BufferedReader(fread);
-			encodedKey = bread.readLine();
-			bread.close();
-			
-			byte[] decodedKey = Base64.getDecoder().decode(encodedKey);
-			SecretKey key = new SecretKeySpec(decodedKey, 0, decodedKey.length, "HmacSHA1"); 
-			
-			// Create and initialize a MAC with the key
-			Mac mac = Mac.getInstance("HmacSHA1");
-			mac.init(key);
-			byte[] buffer = new byte[8192];
-			int length;
-			
-			while ((length = in.read(buffer)) != -1)
-				  mac.update(buffer, 0, length);
-			result = mac.doFinal();
-			
-			System.out.println("Mac: " + asHex(result));
-			//System.out.println("Expected: " + asHex(ehmac));
-		} catch (Exception e) {
-			e.printStackTrace();
+	    
+    private static String asHex (byte buf[]) {
+		//Obtain a StringBuffer object
+		StringBuffer strbuf = new StringBuffer(buf.length * 2);
+		int i;
+        
+		for (i = 0; i < buf.length; i++) {
+			if (((int) buf[i] & 0xff) < 0x10)
+				strbuf.append("0");
+			strbuf.append(Long.toString((int) buf[i] & 0xff, 16));
 		}
-		return Arrays.equals(result, ehmac);
+		// Return result string in Hexadecimal format
+		return strbuf.toString();
 	}
 	
-	public static void main(String [] args) throws IOException {
+	public static void main(String [] args) throws Exception {
 		String fname = "image.jpg";
 
 		System.setProperty("javax.net.ssl.trustStore", "securecam.store");
@@ -87,7 +80,8 @@ public class Client {
 			return;
 		}
 		FileOutputStream fos = new FileOutputStream(fname);
-		boolean verified = verify(ehmac);
+		//boolean verified = verify(ehmac);
+		boolean verified = verifySignature(ehmac);
 		try {
 			if (verified){
 				DataInputStream iin = new DataInputStream(new ByteArrayInputStream(CACHE.toByteArray()));;
@@ -118,20 +112,5 @@ public class Client {
 		myMD5.update(bFile, 0, bFile.length);
 		byte[] md = myMD5.digest();
 		System.out.println("MD5 = " +  asHex(md) );
-      
-	}
-    
-    private static String asHex (byte buf[]) {
-		//Obtain a StringBuffer object
-		StringBuffer strbuf = new StringBuffer(buf.length * 2);
-		int i;
-        
-		for (i = 0; i < buf.length; i++) {
-			if (((int) buf[i] & 0xff) < 0x10)
-				strbuf.append("0");
-			strbuf.append(Long.toString((int) buf[i] & 0xff, 16));
-		}
-		// Return result string in Hexadecimal format
-		return strbuf.toString();
 	}
 }
