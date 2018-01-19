07153cfae8455475fbba0f434f56ca9d50d0e67b
Yong Ze <24569832+Infinitide@users.noreply.github.com>
2018-01-20 00:28:06 +0800

    v2 stable
    Added Asymmetric Authentication
    Added KeyPair Generator

--- bin/Server/ServerThread.java
+++ bin/Server/ServerThread.java
@@ -1,18 +1,24 @@
 import java.net.*;
 import java.io.*;
+import java.nio.file.*;
 import java.util.*;
 import java.text.*;
 import javax.net.ssl.*;
+import java.security.*;
 import javax.crypto.*;
 import javax.crypto.spec.*;
+import java.security.spec.*;
 
 public class ServerThread extends Thread{
 	private Socket SOCKET;
-	private byte[] HMAC;
+	private Date START;
 	private ByteArrayOutputStream CACHE;
+	private byte[] SIGNATURE;
+	private final String PRIVATEKEY = "private.key";
 	
 	public ServerThread(Socket socket) {
 		SOCKET = socket;
+		START = new Date();
 	}
 	
 	public void run() {
@@ -38,13 +44,13 @@ public class ServerThread extends Thread{
 			}
 			
 			DateFormat dateFormat = new SimpleDateFormat("yy/MM/dd HH:mm:ss");
-			Date date = new Date();
-			System.out.println("Sending image " + dateFormat.format(date));
-		  
+			System.out.println("Sending image " + dateFormat.format(START));
+			
 			cache(in);
+			sign();
 			ArrayList data = new ArrayList();
 			data.add(CACHE.toByteArray());
-			data.add(HMAC);
+			data.add(SIGNATURE);
 			try{
 				oon.writeObject(data);
 			} catch (Exception e) {
@@ -57,6 +63,7 @@ public class ServerThread extends Thread{
 			} else {
 				System.out.println("Handshake Error");
 			}
+			
 			oon.flush();
 			dos.flush();
 			oon.close();
@@ -67,6 +74,16 @@ public class ServerThread extends Thread{
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private void sign() throws InvalidKeyException, Exception{
+		Signature sig = Signature.getInstance("SHA1withRSA");
+		byte[] keyBytes = Files.readAllBytes(new File(PRIVATEKEY).toPath());
+		PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+		KeyFactory kf = KeyFactory.getInstance("RSA");
+		sig.initSign(kf.generatePrivate(spec));
+		sig.update(CACHE.toByteArray());
+		SIGNATURE = sig.sign();
 	}
 	
 	private void cache(DataInputStream in){
@@ -81,43 +98,12 @@ public class ServerThread extends Thread{
 			baos.flush();
 			
 			CACHE = baos;
-			initMac();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	private void initMac() {
-		DataInputStream in = new DataInputStream(new ByteArrayInputStream(CACHE.toByteArray()));
-		String keyFile = "key.txt";
-		String encodedKey = null;
-		try {
-			FileReader fread = new FileReader(keyFile);
-			BufferedReader bread = new BufferedReader(fread);
-			encodedKey = bread.readLine();
-			bread.close();
-			
-			byte[] decodedKey = Base64.getDecoder().decode(encodedKey);
-			SecretKey key = new SecretKeySpec(decodedKey, 0, decodedKey.length, "HmacSHA1"); 
-			
-			// Create and initialize a MAC with the key
-			Mac mac = Mac.getInstance("HmacSHA1");
-			mac.init(key);
-			byte[] buffer = new byte[8192];
-			int length;
-			
-			while ((length = in.read(buffer)) != -1)
-				  mac.update(buffer, 0, length);
-			HMAC = mac.doFinal();
-			
-			System.out.println("Mac: " + asHex(HMAC));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
-	private String asHex (byte buf[]) {
+	private String asHex(byte buf[]) {
 		//Obtain a StringBuffer object
 		StringBuffer strbuf = new StringBuffer(buf.length * 2);
 		int i;
